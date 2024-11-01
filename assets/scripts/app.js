9158d0c8baea1f4b199c4df7b7698d646d1c8ddf
pradip <pradippatil1543@gmail.com>
2024-11-01 16:49:02 +0530

    added logs to console

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,69 @@
-const defaultValue = 10;
-let currentResult = 0;
-currentResult = currentResult + 10 * 3;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-let operationDescription = "" + currentResult + "+10*3";
-
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-currentResult = add(5, 5);
+function writeLogs(operator, beforeResult, userEnteredValue) {
+  const calDescription = `${beforeResult}${operator}${userEnteredValue}`;
+  outputResult(currentResult, calDescription);
+}
 
-outputResult(currentResult, operationDescription);
+function writeToLogsByObject(operation, prevResult, number, result) {
+  const logEntry = {
+    operation: operation,
+    prevResult: prevResult,
+    number: number,
+    result: result,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+
+}
+
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  writeLogs("+", initialResult, enteredNumber);
+  writeToLogsByObject('+',initialResult,enteredNumber,currentResult);
+
+
+}
+
+function substract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  writeLogs("-", initialResult, enteredNumber);
+  writeToLogsByObject('-',initialResult,enteredNumber,currentResult);
+
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  writeLogs("*", initialResult, enteredNumber);
+  writeToLogsByObject('*',initialResult,enteredNumber,currentResult);
+
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  writeLogs("/", initialResult, enteredNumber);
+  writeToLogsByObject('/',initialResult,enteredNumber,currentResult);
+
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
